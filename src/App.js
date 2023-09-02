e812a802162172937264c0dc2b667d2784ba1515
WilfredoAcosta <wilfredo.acosta10@gmail.com>
2023-09-02 08:54:42 -0600

    Form completo falta factura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { PlusOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from 'antd';
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+const FormDisabledDemo = () => {
+  const [componentDisabled, setComponentDisabled] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        disabled={componentDisabled}
+        style={{
+          maxWidth: 600,
+        }}
+      >
+    
+        <Form.Item label="Input">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Select">
+          <Select>
+            <Select.Option value="panama">Ciudad de Panama</Select.Option>
+            <Select.Option value="mexico">Cancun Mexico</Select.Option>
+            <Select.Option value="chile">Santiago de Chile</Select.Option>
+            <Select.Option value="peru">Machu picchu Peru</Select.Option>
+            <Select.Option value="honduras">Rotan Honduras</Select.Option>
+          </Select>
+        </Form.Item>
+        
 
-export default App;
+       <Form.Item label="Personas: " required>
+          <InputNumber
+            min={1}
+            onKeyPress={(event) => {
+              if (!/[0-9]/.test(event.key)) {
+                event.preventDefault();
+              }
+            }}
+          />
+        </Form.Item>
+
+        
+        <Form.Item label="Button">
+          <Button>Button</Button>
+        </Form.Item>
+        
+      </Form>
+    </>
+  );
+};
+export default () => <FormDisabledDemo />;
