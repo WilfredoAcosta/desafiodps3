9b69e338cb5ef7e99d4448e070c3ebdb83eafe5a
birdkron <js8215001@gmail.com>
2023-09-02 20:06:26 -0600

    agregada data base

---
+++ src/utils/data.js
@@ -0,0 +1,42 @@
+export const destinos = [
+    {
+        nombre: 'Ciudad de Panama',
+        descripcion: 'Ciudad de Panama',
+        impuesto: 4.5,
+        adicional: [
+            197,165,149,129
+        ]
+    },
+    {
+        nombre: 'Cancun Mexico',
+        descripcion: 'Cancun Mexico',
+        impuesto: 6,
+        adicional: [
+            485,439,400,380
+        ]
+    },
+    {
+        nombre: 'Santiago de Chile',
+        descripcion: 'Santiago de Chile',
+        impuesto: 7,
+        adicional: [
+            198,178,158,138
+        ]
+    },
+    {
+        nombre: 'Machu Picchu Perú',
+        descripcion: 'Machu Picchu Perú',
+        impuesto: 7.5,
+        adicional: [
+            754,699,649,629
+        ]
+    },
+    {
+        nombre: 'Roatán Honduras ',
+        descripcion: 'Roatán Honduras ',
+        impuesto: 4,
+        adicional: [
+            565,499,469,449
+        ]
+    },
+]
