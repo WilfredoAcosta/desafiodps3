5511c172792b3169cacf3f44a94c721f7eb6d674
birdkron <js8215001@gmail.com>
2023-09-02 19:53:50 -0600

    refactor componentes base

--- src/App.js
+++ src/App.js
@@ -1,97 +1,71 @@
-import { PlusOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import {
   Button,
-  Cascader,
-  Checkbox,
-  DatePicker,
   Form,
-  Input,
   InputNumber,
-  Radio,
   Select,
-  Divider,
   Typography,
   Space
 } from 'antd';
-const { RangePicker } = DatePicker;
-const { Paragraph } = Typography;
-const { TextArea } = Input;
-const normFile = (e) => {
-  if (Array.isArray(e)) {
-    return e;
-  }
-  return e?.fileList;
-};
+import Factura from './components/Factura';
 const FormDisabledDemo = () => {
-  const [componentDisabled, setComponentDisabled] = useState(false);
+
+  const [destino, setDestino] = useState('');
+  const [cantidadPersonas, setCantidadPersonas] = useState(0);
+  const [costoPorPersona, setCostoPorPersona] = useState(0);
+  const [impuestosPorPersona, setImpuestosPorPersona] = useState(0);
+  const [totalPagar, setTotalPagar] = useState(0);
+
   return (
     <>
-      <Space direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
-      <Form
-        layout="horizontal"
-        style={{
-          maxWidth:700
-        }
-        }
-      >
-               <Form.Item label=" ">
-               <Typography.Title
-        level={1}
-        style={{
-          margin: 0,
-          textAlign:'center'
-        }}
-      >
-       UBD Tours
-      </Typography.Title>
-        </Form.Item>
- 
-        <Form.Item label="Destino">
-          <Select>
-            <Select.Option value="panama">Ciudad de Panama</Select.Option>
-            <Select.Option value="mexico">Cancun Mexico</Select.Option>
-            <Select.Option value="chile">Santiago de Chile</Select.Option>
-            <Select.Option value="peru">Machu picchu Peru</Select.Option>
-            <Select.Option value="honduras">Rotan Honduras</Select.Option>
-          </Select>
-        </Form.Item>
-        
+      <Space direction="horizontal" style={{ width: '100%', justifyContent: 'center' }}>
+        <Form
+          layout="horizontal"
+          style={{
+            maxWidth: 700
+          }
+          }
+        >
+          <Form.Item label=" ">
+            <Typography.Title
+              level={1}
+              style={{
+                margin: 0,
+                textAlign: 'center'
+              }}
+            >
+              UBD Tours
+            </Typography.Title>
+          </Form.Item>
 
-       <Form.Item label="Personas: " required>
-          <InputNumber
-            min={1}
-            onKeyPress={(event) => {
-              if (!/[0-9]/.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-          />
-        </Form.Item>
+          <Form.Item label="Destino">
+            <Select>
+              <Select.Option value="panama">Ciudad de Panama</Select.Option>
+              <Select.Option value="mexico">Cancun Mexico</Select.Option>
+              <Select.Option value="chile">Santiago de Chile</Select.Option>
+              <Select.Option value="peru">Machu picchu Peru</Select.Option>
+              <Select.Option value="honduras">Roatan Honduras</Select.Option>
+            </Select>
+          </Form.Item>
 
-        
-        <Form.Item label="Button">
-          <Button>Button</Button>
-        </Form.Item>
-        <Form.Item label=" ">
-               <Typography.Title
-        level={1}
-        style={{
-          margin: 0,
-          textAlign:'center'
-        }}
-      >
-       Factura
-      </Typography.Title>
-      <Input placeholder='Destino vacacional' disabled />
-      <Input placeholder='Cantidad de personas' disabled />
-      <Input placeholder='Costo por persona' disabled />
-      <Input placeholder='Impuesto por persona' disabled />
-      <Input placeholder='Total a pagar' disabled />
-        </Form.Item>
+          <Form.Item label="Personas: " required>
+            <InputNumber
+              min={1}
+              onKeyPress={(event) => {
+                if (!/[0-9]/.test(event.key)) {
+                  event.preventDefault();
+                }
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="Button">
+            <Button>Button</Button>
+          </Form.Item>
+        </Form>
 
-      </Form>
       </Space>
+      <Factura destino={destino} cantidadPersonas={cantidadPersonas}costoPorPersona={costoPorPersona}
+      impuestosPorPersona={impuestosPorPersona} totalPagar={totalPagar} ></Factura>
     </>
   );
 };
